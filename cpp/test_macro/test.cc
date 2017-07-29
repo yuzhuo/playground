ea52ca7d6c6b281d748e6043b86f259f2c8c6193
yuzhuo <goodcomrade@hotmail.com>
2017-07-29 10:52:01 +0800

    mv cpp source files to cpp directory

---
+++ cpp/test_macro/test.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << __func__ << endl;
+    cout << __DATE__ << endl;
+    cout << __TIME__ << endl;
+    cout << __FILE__ << endl;
+    cout << __cplusplus << endl;
+    cout << __LINE__ << endl;
+
+    return 0;
+}
